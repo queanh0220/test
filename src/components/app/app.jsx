de0ca770fc7b5e2c62bfd4c8261406d3431be862
queanh0220 <queanh0220@gmail.com>
2022-04-26 11:49:17 +0700

    test deploy

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import "./app.css";
 
 export class App extends React.Component {
   render() {
     const { children } = this.props;
-
     return (
       <div className="app">
+        <Link to="/movies">
+          <button>Movies</button>
+        </Link>
         {children}
         <Outlet />
       </div>
